bba80594121968b24c1f33712e8ee8ed20f83984
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2023-02-21 10:02:20 -0300

    implemented view binding

--- app/src/main/java/com/example/gasto_combustivel/MainActivity.kt
+++ app/src/main/java/com/example/gasto_combustivel/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.gasto_combustivel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.gasto_combustivel.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        Toast.makeText(this, "Fui clicado!", Toast.LENGTH_LONG).show()
     }
 }
+
